30b4b394ad9bf52e3fa4960a162980e613969923
Mark Varadi <mvaradi@avolvesoftware.com>
2023-04-10 23:11:42 +0300

    added MongoConfig for encryption

---
+++ mongo-demo/mongo-demo/src/main/java/ro/fasttrackit/mongodemo/config/MongoConfig.java
@@ -0,0 +1,50 @@
+package ro.fasttrackit.mongodemo.config;
+
+import com.mongodb.client.MongoClient;
+import com.mongodb.client.MongoClients;
+import org.jasypt.encryption.StringEncryptor;
+import org.jasypt.encryption.pbe.StandardPBEStringEncryptor;
+import org.jasypt.iv.RandomIvGenerator;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.mongodb.MongoDatabaseFactory;
+import org.springframework.data.mongodb.core.MongoTemplate;
+import org.springframework.data.mongodb.core.SimpleMongoClientDatabaseFactory;
+
+@Configuration
+public class MongoConfig {
+    @Value("${spring.data.mongodb.uri}")
+    private String mongoUri;
+
+    @Value("${spring.data.mongodb.database}")
+    private String database;
+
+    @Value("${mongodb.encryptedPassword}")
+    private String encryptedPassword;
+
+    @Bean
+    public StringEncryptor jasyptStringEncryptor() {
+        StandardPBEStringEncryptor encryptor = new StandardPBEStringEncryptor();
+        encryptor.setAlgorithm("PBEWithHMACSHA512AndAES_256");
+        encryptor.setIvGenerator(new RandomIvGenerator());
+        encryptor.setPassword(System.getenv("JASYPT_ENCRYPTOR_PASSWORD"));
+        return encryptor;
+    }
+
+    @Bean
+    public MongoDatabaseFactory mongoDatabaseFactory(StringEncryptor jasyptStringEncryptor) {
+        String decryptedPassword = jasyptStringEncryptor.decrypt(encryptedPassword);
+        String connectionString = mongoUri.replace("<DECRYPTED_PASSWORD>", decryptedPassword);
+        MongoClient mongoClient = MongoClients.create(connectionString);
+        return new SimpleMongoClientDatabaseFactory(mongoClient, database);
+    }
+
+    @Bean
+    public MongoTemplate mongoTemplate(MongoDatabaseFactory mongoDatabaseFactory) {
+        return new MongoTemplate(mongoDatabaseFactory);
+    }
+}
+
+
+
